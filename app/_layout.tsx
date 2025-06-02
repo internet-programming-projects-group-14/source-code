4475593163eaf60e3f5b93c7bed6593aee252e10
DIONEMELINA <dionemelina20@gmail.com>
2025-06-02 18:20:48 +0100

    Implementation of the settings screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,6 +5,7 @@ import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
+import React from 'react';
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
@@ -20,8 +21,8 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
+        <Stack.Screen name = "index" options = {{headerShown: false}}/>
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
